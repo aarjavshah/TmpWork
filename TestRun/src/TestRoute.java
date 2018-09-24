bb8e275a12a8d1aada808ff327294a6865ab610a
aarjavshah <arjav.d.shah@gmail.com>
2018-09-24 23:36:29 +0200

    Initial check-in

---
+++ TestRun/src/TestRoute.java
@@ -0,0 +1,48 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Servlet implementation class TestRoute
+ */
+@WebServlet("/checkStatus")
+public class TestRoute extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		response.getWriter().append("{\"Response\":\"It Works!\"}");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 StringBuilder buffer = new StringBuilder();
+		    BufferedReader reader = request.getReader();
+		    String line;
+		    while ((line = reader.readLine()) != null) {
+		        buffer.append(line);
+		    }
+		    String data = buffer.toString();
+		    JSONObject json = null;
+		    try {
+				 json = new JSONObject(data);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		response.setContentType("application/json");
+		data = json.toString();
+		response.getWriter().append(data);
+	}
+
+}
